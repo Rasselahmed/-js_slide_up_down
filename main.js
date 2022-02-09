46b22c33dce02997276ecc0842cd7d6ff7c93952
Rassel Ahmed <94746949+Rasselahmed@users.noreply.github.com>
2022-02-09 12:27:12 +0600

    Add files via upload
    upload

---
+++ main.js
@@ -0,0 +1,18 @@
+//slideup
+//slidedown
+//slideggle
+
+
+
+$(document).ready(function () {
+    $('#slideup').click(function () {
+        $('.box').slideUp(3000);
+    });
+
+    $('#slidedown').click(function () {
+        $('.box').slideDown(3000);
+    });
+
+
+
+});
